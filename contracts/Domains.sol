d7b0d4e8cd7e0b2d815fbf2999bfaa9485c36905
Yukai Huang <yukaihuangtw@gmail.com>
2022-02-20 11:52:17 +0800

    The error example

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -1,25 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
-
 pragma solidity ^0.8.10;
 
 import "hardhat/console.sol";
 
 contract Domains {
-  // A "mapping" data type to store their names
   mapping(string => address) public domains;
+  
+  // Checkout our new mapping! This will store values
+  mapping(string => string) public records;
 
   constructor() {
-      console.log("THIS IS MY DOMAIN CONTRACT. NICE.");
+      console.log("Yo yo, I am a contract and I am smart");
   }
 
-  // A register function that adds their names to our mapping
   function register(string calldata name) public {
+      // Check that the name is unregistered (explained in notes)
+      require(domains[name] == address(0));
       domains[name] = msg.sender;
       console.log("%s has registered a domain!", msg.sender);
   }
 
-  // This will give us the domain owners' address
   function getAddress(string calldata name) public view returns (address) {
+      // Check that the owner is the transaction sender
       return domains[name];
+  }
+
+  function setRecord(string calldata name, string calldata record) public {
+      // Check that the owner is the transaction sender
+      require(domains[name] == msg.sender);
+      records[name] = record;
+  }
+
+  function getRecord(string calldata name) public view returns(string memory) {
+      return records[name];
   }
 }
