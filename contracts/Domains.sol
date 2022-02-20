c77525a32509ec746b4f9efe5d54567dc9f28e44
Yukai Huang <yukaihuangtw@gmail.com>
2022-02-20 11:58:38 +0800

    Register address

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -1,24 +1,45 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
+import { StringUtils } from "./libraries/StringUtils.sol";
 import "hardhat/console.sol";
 
 contract Domains {
+  // Here's our domain TLD!
+  string public tld;
+
   mapping(string => address) public domains;
-  
-  // Checkout our new mapping! This will store values
   mapping(string => string) public records;
 
-  constructor() {
-      console.log("Yo yo, I am a contract and I am smart");
+  constructor(string memory _tld) payable {
+    tld = _tld;
+    console.log("%s name service deployed", _tld);
   }
 
-  function register(string calldata name) public {
-      // Check that the name is unregistered (explained in notes)
-      require(domains[name] == address(0));
-      domains[name] = msg.sender;
-      console.log("%s has registered a domain!", msg.sender);
+  // This function will give us the price of a domain based on length
+  function price(string calldata name) public pure returns(uint) {
+    uint len = StringUtils.strlen(name);
+    require(len > 0);
+    if (len == 3) {
+      return 5 * 10**17; // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). We're going with 0.5 Matic cause the faucets don't give a lot
+    } else if (len == 4) {
+      return 3 * 10**17; // To charge smaller amounts, reduce the decimals. This is 0.3
+    } else {
+      return 1 * 10**17;
+    }
   }
+
+  function register(string calldata name) public payable {
+    require(domains[name] == address(0));
+    
+    uint _price = price(name);
+
+    // Check if enough Matic was paid in the transaction
+    require(msg.value >= _price, "Not enough Matic paid");
+
+    domains[name] = msg.sender;
+    console.log("%s has registered a domain!", msg.sender);
+  }  
 
   function getAddress(string calldata name) public view returns (address) {
       // Check that the owner is the transaction sender
@@ -34,4 +55,6 @@ contract Domains {
   function getRecord(string calldata name) public view returns(string memory) {
       return records[name];
   }
+
+
 }
