0a0b43553bffaa0a645064151588dd6b0b97562a
Yukai Huang <yukaihuangtw@gmail.com>
2022-02-20 11:24:58 +0800

    feat: basic domain

---
+++ scripts/run.js
@@ -0,0 +1,26 @@
+const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
+  const domainContractFactory = await hre.ethers.getContractFactory('Domains');
+  const domainContract = await domainContractFactory.deploy();
+  await domainContract.deployed();
+  console.log("Contract deployed to:", domainContract.address);
+  console.log("Contract deployed by:", owner.address);
+  
+  const txn = await domainContract.register("doom");
+  await txn.wait();
+
+  const domainOwner = await domainContract.getAddress("doom");
+  console.log("Owner of domain:", domainOwner);
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
