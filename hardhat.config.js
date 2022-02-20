32899946dd14927ddb0743e7f8e41950cc7d9b45
Yukai Huang <yukaihuangtw@gmail.com>
2022-02-20 12:14:02 +0800

    Deploy to mumbai

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,4 +18,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.10",
+  networks: {
+    mumbai: {
+      url: process.env.ALCHEMY_API_URL,
+      accounts: [
+        process.env.TESTER_PRIVATE_KEY
+      ],
+		}
+  }
 };
